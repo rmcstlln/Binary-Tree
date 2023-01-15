926dbecb793d229208489268fda69fe58dfa4c33
rmcstlln <romarkpinero68@gmail.com>
2023-01-15 09:11:10 +0800

    Defining a funtion in_order_traversal

--- Binary_Tree_Part1.py
+++ Binary_Tree_Part1.py
@@ -20,3 +20,14 @@ class BinarySearchTreeNode:
                 self.right.add_child(data)
             else:
                 self.right = BinarySearchTreeNode(data)
+
+    def in_order_traversal(self):
+        elements = []
+
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
