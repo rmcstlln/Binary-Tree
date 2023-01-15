75bcdeeba3c6b058a3556ecb6bd21dd28cf7f4e5
rmcstlln <romarkpinero68@gmail.com>
2023-01-15 09:28:36 +0800

    Defining a function search

--- Binary_Tree_Part1.py
+++ Binary_Tree_Part1.py
@@ -34,6 +34,22 @@ class BinarySearchTreeNode:
 
         return elements
 
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
 
 def build_tree(elements):
     root = BinarySearchTreeNode(elements[0])
