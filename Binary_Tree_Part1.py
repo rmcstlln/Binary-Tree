ff971a720042446fcb21e56867a4697a081d289b
rmcstlln <romarkpinero68@gmail.com>
2023-01-15 09:08:36 +0800

    Defining a function add_child

--- Binary_Tree_Part1.py
+++ Binary_Tree_Part1.py
@@ -3,3 +3,20 @@ class BinarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
+
+        if data < self.data:
+            # add data in left subtree
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
