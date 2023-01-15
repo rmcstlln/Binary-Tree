827435f6c75029afe3d0fea496c3c8eeadc67a52
rmcstlln <romarkpinero68@gmail.com>
2023-01-15 09:24:22 +0800

    Fixing Error and Example 1

--- Binary_Tree_Part1.py
+++ Binary_Tree_Part1.py
@@ -32,6 +32,8 @@ class BinarySearchTreeNode:
         if self.right:
             elements += self.right.in_order_traversal()
 
+        return elements
+
 
 def build_tree(elements):
     root = BinarySearchTreeNode(elements[0])
@@ -40,3 +42,10 @@ def build_tree(elements):
         root.add_child(elements[i])
 
     return root
+
+
+if __name__ == '__main__':
+    # Example 1
+    numbers = [17, 4, 1, 20, 9, 23, 18, 34]
+    numbers_tree = build_tree(numbers)
+    print(numbers_tree.in_order_traversal())
